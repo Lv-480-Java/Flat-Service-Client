33d0b97ea829454c435c6a4da037275f754d4714
maksym-huliaka <maxim.lviv1@gmail.com>
2020-03-04 13:38:38 +0200

    flat list, flat search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,3 @@
-
-import { ProfileComponent } from './profile/profile.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './app-routing.module';
@@ -7,31 +5,57 @@ import {AppComponent} from './app.component';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { RegistrationComponent } from './registration/registration.component';
+import {HttpClientModule} from '@angular/common/http';
 import {LoginComponent} from './login/login.component';
 import {HttpService} from './services/http.service';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
-
+import { FlatListComponent } from './flat-list/flat-list.component';
+import { MatSliderModule } from '@angular/material/slider';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import {MatChipsModule} from '@angular/material/chips';
+import { FlatFilterComponent } from './flat-filter/flat-filter.component';
+import { Ng5SliderModule } from 'ng5-slider';
+import {FlexModule} from '@angular/flex-layout';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatTableModule} from '@angular/material/table';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {ProfileComponent} from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
     RegistrationComponent,
-    LoginComponent
-
+    LoginComponent,
+    AppComponent,
+    FlatListComponent,
+    FlatFilterComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     BrowserAnimationsModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    Ng5SliderModule,
+    MatSliderModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    FlexModule,
+    InfiniteScrollModule,
+    MatChipsModule,
+    MatTableModule,
+    MatCheckboxModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
