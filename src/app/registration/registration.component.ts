3c3d59bdbf3d927fac76c31969c4fa6b18f8f72a
Roman Blavatskyi <roman.blavatskyi@gmail.com>
2020-03-05 12:45:08 +0200

    modified registration page

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {User} from '../User';
 import {HttpUserService} from '../services/http.user.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -8,19 +9,45 @@ import {HttpUserService} from '../services/http.user.service';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
+  form: FormGroup;
 
   user: User = new User();
   done = false;
-  constructor(private httpService: HttpUserService) {}
+
+  constructor(private httpService: HttpUserService) {
+  }
+
   submit(user: User) {
     this.httpService.postData(user)
       .subscribe(
-        (data: User) => {this.done = true; },
+        (data: User) => {
+          this.done = true;
+        },
         error => console.log(error)
       );
   }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      username: new FormControl('', [
+        Validators.required]),
+      email: new FormControl(null, [
+        Validators.email,
+        Validators.required]),
+      phoneNumber: new FormControl('', [
+        Validators.required
+      ]),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6)
+      ])
+    });
   }
 
+  /*  submit() {
+      console.log("Form submitted: ", this.form)
+      const formData = {...this.form.value}
+      console.log("Form Data", formData)
+    }*/
 }
