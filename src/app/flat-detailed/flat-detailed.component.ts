37781a2205d29ba3c6cde9ac6e85e38864bb8b1a
maksym-huliaka <maxim.lviv1@gmail.com>
2020-03-05 19:02:06 +0200

    minor changes

--- src/app/flat-detailed/flat-detailed.component.ts
+++ src/app/flat-detailed/flat-detailed.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {GalleryItem, ImageItem} from '@ngx-gallery/core';
+import {ActivatedRoute} from '@angular/router';
+import {HttpClient} from '@angular/common/http';
+import {FlatDetailed} from './entity/FlatDetailed';
 
 @Component({
   selector: 'app-flat-detailed',
@@ -8,19 +11,38 @@ import {GalleryItem, ImageItem} from '@ngx-gallery/core';
 })
 export class FlatDetailedComponent implements OnInit {
 
-  constructor() {
+  constructor(private route: ActivatedRoute, private http: HttpClient) {
   }
 
-
   images: GalleryItem[];
+  private id: number;
+  data: any;
+  public flatDetailed: FlatDetailed = new FlatDetailed();
 
   ngOnInit(): void {
-    this.images = [
-      new ImageItem({src: 'https://www.thebigflatbarcelona.com/wp-content/uploads/2016/12/bigflathomebarcelona.jpg', thumb: 'IMAGE_THUMBNAIL_URL'}),
-      new ImageItem({src: 'https://www.thebigflatbarcelona.com/wp-content/uploads/2016/12/MG_8339-e1542377773910.jpg', thumb: 'IMAGE_THUMBNAIL_URL'}),
-      new ImageItem({src: 'https://www.thebigflatbarcelona.com/wp-content/uploads/2016/12/full-yellow-e1542377993284.jpg', thumb: 'IMAGE_THUMBNAIL_URL'}),
-      new ImageItem({src: 'https://www.thebigflatbarcelona.com/wp-content/uploads/2016/12/BARCELONA-FLAT-RAVAL00227.jpg', thumb: 'IMAGE_THUMBNAIL_URL'})
-    ];
-}
+    this.route.paramMap.subscribe(params => {
+      this.id = Number(params.get('id'));
+    });
+    this.loadFlat();
+  }
 
+  loadFlat(): void {
+    const c = 'http://localhost:8080/flat/' + this.id;
+    this.http.get(c)
+      .subscribe(data => {
+        this.data = data;
+        this.flatDetailed = this.data;
+        this.loadImages();
+      });
+  }
+
+  loadImages(): void {
+    this.images = [];
+    this.flatDetailed.photos.forEach((value) => {
+      this.images.push( new ImageItem({
+        src: value,
+        thumb: value
+      }));
+    });
+  }
 }
