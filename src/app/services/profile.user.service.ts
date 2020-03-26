6ec320f9a1be6f89ae0713f6f653a3970ff9f248
Latyk87 <boris21352@gmail.com>
2020-03-27 00:33:44 +0200

    Added field validation for Passport component

--- src/app/services/profile.user.service.ts
+++ src/app/services/profile.user.service.ts
@@ -11,15 +11,11 @@ import {catchError} from 'rxjs/operators';
 export class ProfileUserService {
   private options = {headers: new HttpHeaders().set('Content-Type', 'application/json')};
   public error$: Subject<string> = new Subject<string>();
-  public error_username$: Subject<string> = new Subject<string>();
-  public error_email$: Subject<string> = new Subject<string>();
-  public error_phone$: Subject<string> = new Subject<string>();
-  public error_password$: Subject<string> = new Subject<string>();
+  public ERROR_USERNAME$: Subject<string> = new Subject<string>();
+  public ERROR_PHONE$: Subject<string> = new Subject<string>();
 
   constructor(private http: HttpClient) {
   }
-
-  private id = () => JSON.parse(localStorage.getItem('user')).userId;
 
   addUserData(): Observable<User> {
     return this.http.get<User>(BASE_URL + 'users/currentUser');
@@ -27,9 +23,9 @@ export class ProfileUserService {
 
   updateUserData(userData: User): Observable<User> {
     return this.http.put<User>(BASE_URL + 'users/update/all', JSON.stringify(userData), this.options)
-  .pipe(
-      catchError(this.handleError.bind(this))
-    );
+      .pipe(
+        catchError(this.handleError.bind(this))
+      );
   }
 
   evaluateToLandlord(dataLand: User): Observable<User> {
@@ -46,19 +42,10 @@ export class ProfileUserService {
     console.log(error);
     if (message instanceof Object) {
       if (message.username !== undefined) {
-        this.error_username$.next(message.username);
+        this.ERROR_USERNAME$.next('Please input nick name');
       }
-
-      if (message.email !== undefined) {
-        this.error_email$.next(message.email);
-      }
-
       if (message.phoneNumber !== undefined) {
-        this.error_phone$.next('Please input correct phone number');
-      }
-
-      if (message.password !== undefined) {
-        this.error_password$.next(message.password);
+        this.ERROR_PHONE$.next('Please input correct phone number');
       }
     } else {
       this.error$.next(message);
