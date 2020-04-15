3ec19891f95db9f211558eb0613b0729d601b873
AndriyPyzh <andriy.pyzh2412@gmail.com>
2020-04-15 16:38:15 +0300

    add active-counts component

---
+++ src/app/admin-panel/dashboard-page/active-counts/active-counts.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-active-counts',
+  templateUrl: './active-counts.component.html',
+  styleUrls: ['./active-counts.component.scss']
+})
+export class ActiveCountsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
