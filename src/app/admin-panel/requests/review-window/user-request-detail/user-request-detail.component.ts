2e82541ea004b6a861bdf5327759b91edaf5bd9e
AndriyPyzh <andriy.pyzh2412@gmail.com>
2020-05-05 20:45:35 +0300

    renters requests fixed

--- src/app/admin-panel/requests/review-window/user-request-detail/user-request-detail.component.ts
+++ src/app/admin-panel/requests/review-window/user-request-detail/user-request-detail.component.ts
@@ -8,32 +8,41 @@ import {User} from '../../../component/Users';
   styleUrls: ['./user-request-detail.component.scss']
 })
 export class UserRequestDetailComponent implements OnInit {
-
+  noPhoto =  'https://s3.eu-central-1.amazonaws.com/makler.tabbo/photo_2020-04-28_20-46-46.jpg';
   data: Landlord;
   userData: User;
-  @Input() id: number;
-
-  constructor(private landlordService: ProfileService) {
+  constructor(public landlordService: ProfileService) {
   }
-
-  ngOnInit() {
+  ngOnInit()  {
     this.addPassport();
     this.addUserInfo();
   }
-
+  resetData() {
+    this.landlordService.error$.next('');
+    this.data.firstName = '';
+    this.data.lastName = '';
+    this.data.authority = '';
+    this.data.birthDate = '';
+    this.data.birthPlace = '';
+    this.data.dateOfIssue = '';
+    this.data.expirationDate = '';
+    this.data.gender = '';
+    this.data.identificationNumber = null;
+    this.data.middleName = '';
+    this.data.nationality = '';
+    this.data.passportNumber = '';
+    this.data.passportType = '';
+  }
   addPassport() {
-    this.landlordService.getPassportByUser(this.id)
+    this.landlordService.addPassport()
       .subscribe(data => {
         this.data = data;
       });
   }
-
   addUserInfo() {
-    this.landlordService.getUserInfo(this.id)
+    this.landlordService.addUserInfo()
       .subscribe(userData => {
         this.userData = userData;
       });
   }
-
-
 }
